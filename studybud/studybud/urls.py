143ea7fd543fdd30bb44e66345591afde41c3a61
ChandrashekharRobbi <chandrashekarrobbi789@gmail.com>
2023-09-23 13:47:05 +0530

    urls 🔗

---
+++ studybud/studybud/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+from django.http import HttpResponse
+from base import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('base.urls'))
+]
